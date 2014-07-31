0cb902b821eb7b67b9b6b02dcb60f15e774251ed
Robin Kauffman <robink@creosotehill.org>
2014-07-31 13:21:20 -0700

    Update ClojureScript dependency and define route.
    Update ClojureScript to -2277 in project.clj (probably not current any
    more, but close enough), and define a stupid route in
    src/csjspg/core.clj since having a route is (assumedly) better than
    having nothing.

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-  		 [org.clojure/clojurescript "0.0-2173"]
+  		 [org.clojure/clojurescript "0.0-2227"]
 		 [compojure "1.1.8"]
-		 [ring "1.3.0-beta2"]
+		 [ring "1.3.0"]
 		 [enliven "0.1.0-SNAPSHOT"]
 		 [com.phloc/phloc-css "3.7.7"]
 		 [org.jsoup/jsoup "1.7.3"]
@@ -16,3 +16,4 @@
 		 [om "0.6.2"]]
   :profiles {:dev [:dbmunge :datomicdb]}
   :repositories [["CHS3" {:url "http://rms3.creosotehill.org/mirror/maven/" :snapshots true}]])
+
--- src/csjspg/core.clj
+++ src/csjspg/core.clj
@@ -1,6 +1,4 @@
-(ns csjspg.core)
+(ns csjspg.core (:require [clojure.java.jdbc :as jdbc] [clojure.java.io :as io] [hiccup.core :as hiccup] [hiccup.page :as hp] [ring.util.servlet :as rs] [ring.adapter.jetty :as rj] [compojure.route :as cr] [compojure.core :as cc :refer [ANY GET POST PUT defroutes let-routes routes]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defroutes base (ANY "/some/:thing" [thing] {:headers {"Content-Type" "text/html"} :response 200 :body (hp/html5 [:html [:head [:title "Well, isn't this something?"]] [:body [:h2 "Welcome to " thing]]])})
+                (ANY "*" [request] {:response 501 :headers {"Content-Type" "text/plain"} :body (str "Sorry, we don't have an API for that REST request implemented.  Here's your request map:\n\n" (pr-str request))}))
